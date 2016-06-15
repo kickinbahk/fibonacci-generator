c8fec0925bf58062997b762ba107a6050369e04b
kickinbahk <kickinbahk@gmail.com>
2016-06-15 07:41:42 -0700

    test for 1 number

--- fibonacci-generator.spec.js
+++ fibonacci-generator.spec.js
@@ -1,7 +1,14 @@
-var fibonacciGenerator = require('./fibonacci-generator')
+/*eslint-disable
+space-before-function-paren,
+quotes, semi, no-undef,
+space-before-function-paren,
+padded-blocks */
+
+var FibonacciGenerator = require('./fibonacci-generator')
 
 describe('Fibonacci Genenerator', function () {
   it('returns number if given one number and told to add only one place', function () {
-
+    var fibonacciGen = new FibonacciGenerator(1, 1)
+    expect(fibonacciGen.compute).toEqual(1)
   })
 })
