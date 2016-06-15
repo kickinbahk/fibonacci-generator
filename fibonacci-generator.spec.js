daf95e3e3387b60ef2f6f3f9606644377d470f1b
kickinbahk <kickinbahk@gmail.com>
2016-06-15 07:33:48 -0700

    add first test

---
+++ fibonacci-generator.spec.js
@@ -0,0 +1,7 @@
+var fibonacciGenerator = require('./fibonacci-generator')
+
+describe('Fibonacci Genenerator', function () {
+  it('returns number if given one number and told to add only one place', function () {
+
+  })
+})
