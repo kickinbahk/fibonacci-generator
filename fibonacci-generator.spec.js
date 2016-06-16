c9e0a9b59e9d55fede15078fe34b638a1eb35104
kickinbahk <kickinbahk@gmail.com>
2016-06-16 08:55:27 -0700

    add new test for 3 places

--- fibonacci-generator.spec.js
+++ fibonacci-generator.spec.js
@@ -7,14 +7,19 @@ padded-blocks */
 var FibonacciGenerator = require('./fibonacci-generator')
 
 describe('Fibonacci Genenerator', function () {
-  it('returns number if given one number and told to add only one place',
+  it('if given 1 and 1, will return fibonacci sequenece up to 1 places',
   function () {
     var fibonacciGen = new FibonacciGenerator()
     expect(fibonacciGen.compute(1, 1)).toEqual([0, 1])
   })
-  it('returns number and sum of previous number when given 2 numbers',
+  it('if given 1 and 2, will return fibonacci sequenece up to 2 places',
   function () {
     var fibonacciGen = new FibonacciGenerator()
     expect(fibonacciGen.compute(1, 2)).toEqual([0, 1, 1, 2])
+  })
+  it('if given 1 and 3, will return fibonacci sequence up to 3 places',
+  function () {
+    var fibonacciGen = new FibonacciGenerator()
+    expect(fibonacciGen.compute(1, 3)).toEqual([0, 1, 1, 2, 4])
   })
 })
