9aa1b6e5c47cc6a66237d985452f0180e4c69f69
kickinbahk <kickinbahk@gmail.com>
2016-06-16 12:42:10 -0700

    add test for 4 unique nubmers and refactor test for 1 nubmer for consistency

--- fibonacci-generator.js
+++ fibonacci-generator.js
@@ -3,9 +3,6 @@ var FibonacciGenerator = function () {}
 FibonacciGenerator.prototype.compute = function (endingNum) {
   var sequence = [0, 1]
   for (var x = sequence[1]; x <= endingNum; x++) {
-    if (endingNum === 1) {
-      return sequence
-    }
     var sum = sequence[x] + sequence[x - 1]
     sequence.push(sum)
   }
--- fibonacci-generator.spec.js
+++ fibonacci-generator.spec.js
@@ -9,22 +9,22 @@ var FibonacciGenerator = require('./fibonacci-generator')
 describe('Fibonacci Genenerator', function () {
   var fibonacciGen = new FibonacciGenerator()
 
-  // tests for 1 place
-  it('if given 1, will return fibonacci sequenece up to 1 places',
+  it('if given 1, will return fibonacci sequence to 1',
   function () {
-    expect(fibonacciGen.compute(1)).toEqual([0, 1])
+    expect(fibonacciGen.compute(1)).toEqual([0, 1, 1])
   })
 
-  // tests for 2 places
-  it('if given 2 will return fibonacci sequenece up to 2 places',
+  it('if given 2 will return fibonacci sequence up to 2 different numbers',
   function () {
     expect(fibonacciGen.compute(2)).toEqual([0, 1, 1, 2])
   })
 
-
-  // tests for 3 places
-  it('if given 3, will return fibonacci sequence up to 3 places',
+  it('if given 3, will return fibonacci sequence up to 3 different numbers',
   function () {
     expect(fibonacciGen.compute(3)).toEqual([0, 1, 1, 2, 3])
+  })
+
+  it('if given 4, will return sequence up to 4 numbers', function () {
+    expect(fibonacciGen.compute(4)).toEqual([0, 1, 1, 2, 3, 5])
   })
 })
