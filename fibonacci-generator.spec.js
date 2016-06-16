2f869e2389c97e222f52ccfa7d69abec859609d8
kickinbahk <kickinbahk@gmail.com>
2016-06-16 09:34:00 -0700

    refactor tests for 2 places

--- fibonacci-generator.spec.js
+++ fibonacci-generator.spec.js
@@ -14,15 +14,17 @@ describe('Fibonacci Genenerator', function () {
   function () {
     expect(fibonacciGen.compute(1, 1)).toEqual([0, 1])
   })
-  it('if given 2 and 1, will return fib sequence starting at 2 up to 1 place', function () {
+  it('if given 2 and 1, will return fib sequence starting at 2, up to 1 place', function () {
     expect(fibonacciGen.compute(2, 1)).toEqual([0, 2])
   })
 
   // tests for 2 places
   it('if given 1 and 2, will return fibonacci sequenece up to 2 places',
   function () {
-    var fibonacciGen = new FibonacciGenerator()
     expect(fibonacciGen.compute(1, 2)).toEqual([0, 1, 1, 2])
+  })
+  it('if given 2 and 2, will return fib sequence starting at 2, up to 2 places', function () {
+    expect(fibonacciGen.compute(2, 2)).toEqual([0, 2, 2, 4])
   })
 
   // tests for 3 places
